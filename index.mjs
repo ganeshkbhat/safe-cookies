6b635a6c15b00352a2905ca92884d9ca053c6b0c
Ganesh B <ganeshsurfs@gmail.com>
2024-02-11 20:16:36 +0530

    [COMMIT] Add index.mjs to package

---
+++ index.mjs
@@ -0,0 +1,22 @@
+/**
+ * 
+ * Package: safecookie
+ * Author: Ganesh B
+ * Description: 
+ * Install: npm i safecookie --save
+ * Github: https://github.com/ganeshkbhat/
+ * npmjs Link: https://www.npmjs.com/package/
+ * File: index.js
+ * File Description: 
+ * 
+*/
+
+/* eslint no-console: 0 */
+
+'use strict';
+
+import * as safecookie from "./index.js";
+
+export default {
+  ...safecookie
+}
