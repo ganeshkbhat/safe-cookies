35520b02c16bc707b91f0ed505d258a4d5be7e3a
Ganesh B <ganeshsurfs@gmail.com>
2024-03-03 17:33:23 +0530

    [COMMIT] Adding setCookie, getCookie, deleteCookie functions

---
+++ test/index.gettersetter.cookie.js
@@ -0,0 +1,40 @@
+/**
+ * 
+ * Package: safecookie
+ * Author: Ganesh B
+ * Description: 
+ * Install: npm i safecookie --save
+ * Github: https://github.com/ganeshkbhat/
+ * npmjs Link: https://www.npmjs.com/package/
+ * File: test/index.test.js
+ * File Description: 
+ * 
+*/
+
+/* eslint no-console: 0 */
+
+'use strict';
+
+const expect = require('chai').expect;
+
+describe('[cookies tests] Tests to ', () => {
+  let tst, written;
+  before(() => {
+    
+  });
+
+  after(() => { });
+
+  it('should be able to - blanktest', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should be able to - blanktest', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+});
