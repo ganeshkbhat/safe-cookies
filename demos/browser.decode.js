36ed6a8f809958b444120b65c37f2232d2b7f91b
Ganesh B <ganeshsurfs@gmail.com>
2024-03-04 07:03:07 +0530

    [COMMIT] Adding export structure supported with browsers. Add demos for browser supported codes

---
+++ demos/browser.decode.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * Package: safecookie
+ * Author: Ganesh B
+ * Description: 
+ * Install: npm i safecookie --save
+ * Github: https://github.com/ganeshkbhat/
+ * npmjs Link: https://www.npmjs.com/package/
+ * File: demos/encrypt.js
+ * File Description: 
+ * 
+*/
+
+/* eslint no-console: 0 */
+
+'use strict';
