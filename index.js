041fcf2262bc3cbf39f7b17a6d45f0645b5154c0
Ganesh B <ganeshsurfs@gmail.com>
2024-03-04 07:01:01 +0530

    [COMMIT] Adding export structure supported with browsers

--- index.js
+++ index.js
@@ -479,31 +479,31 @@ function getSalt() {
 //     .toString('ascii')
 // }
 
-/**
- * randomString
- * 
- * // Returns a new random alphanumeric string of the given size.
- * // Note: to simplify implementation, the result has slight modulo bias,
- * // because chars length of 62 doesn't divide the number of all bytes
- * // (256) evenly. Such bias is acceptable for most cases when the output
- * // length is long enough and doesn't need to be uniform.
- * // https://www.tabnine.com/code/javascript/functions/crypto/randomBytes?snippet=5f67c3314b42a09e18720865
- *
- * @param {*} size
- * @return {*} 
- */
-function randomString(size) {
-  if (size === 0) {
-    throw new Error('Zero-length randomString is useless.');
-  }
-  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + 'abcdefghijklmnopqrstuvwxyz' + '0123456789';
-  let objectId = '';
-  const bytes = (0, crypto?.randomBytes || randomBytes)(size);
-  for (let i = 0; i < bytes.length; ++i) {
-    objectId += chars[bytes.readUInt8(i) % chars.length];
-  }
-  return objectId;
-}
+// /**
+//  * randomString
+//  * 
+//  * // Returns a new random alphanumeric string of the given size.
+//  * // Note: to simplify implementation, the result has slight modulo bias,
+//  * // because chars length of 62 doesn't divide the number of all bytes
+//  * // (256) evenly. Such bias is acceptable for most cases when the output
+//  * // length is long enough and doesn't need to be uniform.
+//  * // https://www.tabnine.com/code/javascript/functions/crypto/randomBytes?snippet=5f67c3314b42a09e18720865
+//  *
+//  * @param {*} size
+//  * @return {*} 
+//  */
+// function randomString(size) {
+//   if (size === 0) {
+//     throw new Error('Zero-length randomString is useless.');
+//   }
+//   const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + 'abcdefghijklmnopqrstuvwxyz' + '0123456789';
+//   let objectId = '';
+//   const bytes = (0, crypto?.randomBytes || randomBytes)(size);
+//   for (let i = 0; i < bytes.length; ++i) {
+//     objectId += chars[bytes.readUInt8(i) % chars.length];
+//   }
+//   return objectId;
+// }
 
 /**
  * 
@@ -743,8 +743,8 @@ if (!isBrowser()) {
 
   // module.exports.generateJwtSecret = generateJwtSecret;
   // module.exports.nonce = nonce;
+  // module.exports.randomString = randomString;
   module.exports.randomBytes = randomBytes;
-  module.exports.randomString = randomString;
   module.exports.browserEncode = browserEncode;
   module.exports.browserDecode = browserDecode;
 
