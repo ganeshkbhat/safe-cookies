3494b716fc43e3c35f987959b10ffe93903158e9
Ganesh B <ganeshsurfs@gmail.com>
2024-03-09 18:20:35 +0530

    [COMMIT] Adding get set and delete tests for jasmine

---
+++ spec/deletecookies.spec.js
@@ -0,0 +1,26 @@
+
+const { describe, beforeAll, afterAll,  expect, jasmine} = require('jasmine');
+const { getCookie, setCookie, deleteCookie } = require("../index")
+
+describe('[ tests] Tests to ', () => {
+  let tst, written;
+  beforeAll(() => { });
+
+  afterAll(() => { });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    // expect(actual).not.toBe(expected);
+    expect(actual).toBe(expected);
+
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    // expect(actual).not.toBe(expected);
+    expect(actual).toBe(expected);
+  });
+
+});
---
+++ spec/setcookies.spec.js
@@ -0,0 +1,28 @@
+
+// const {describe, beforeAll, afterAll, it, expect, jasmine} = require('jasmine-core').noGlobals();
+const { describe, beforeAll, afterAll,  expect, jasmine} = require('jasmine');
+const { getCookie, setCookie, deleteCookie } = require("../index")
+
+
+describe('[ tests] Tests to ', () => {
+  let tst, written;
+  beforeAll(() => { });
+
+  afterAll(() => { });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    // expect(actual).not.toBe(expected);
+    expect(actual).toBe(expected);
+
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    // expect(actual).not.toBe(expected);
+    expect(actual).toBe(expected);
+  });
+
+});
